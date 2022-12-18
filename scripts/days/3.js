b7fb1d08de4728602c7d8ee1c3ae47aaf30014a8
Keanu David <keanu.david@student.howest.be>
2022-12-18 23:07:35 +0100

    Finished day 3

--- scripts/days/3.js
+++ scripts/days/3.js
@@ -1,3 +1,28 @@
-const sortRucksacks = (content) => {
+const findDuplicateInSack = (sack) => {
+    let duplicate;
+    const firstCompartment = sack.slice(0, sack.length / 2);
+    const secondCompartment = sack.slice(sack.length / 2, sack.length);
 
+    for (let item of firstCompartment) {
+        if (secondCompartment.includes(item)) {
+            duplicate = item;
+            break; // The sack will only contain one duplicate type, once it is found we can stop looping.
+        }
+    }
+
+    return duplicate;
+}
+
+const alphabet = '.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const sortRucksacks = (content) => {
+    const duplicateItems = [];
+    let totalTypeScore = 0;
+
+    // Search rucksacks...
+    const ruckSacks = content.split('\n');
+    ruckSacks.forEach((sack) => duplicateItems.push(findDuplicateInSack(sack)));
+    duplicateItems.forEach((type) => totalTypeScore += alphabet.indexOf(type));
+
+    console.log('Total duplicate type score is', totalTypeScore);
 }
